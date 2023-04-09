5c686e35b985bbcbd68be7d68569221a677a11e6
israelsanttana <israelsantanasantos7@gmail.com>
2023-04-09 18:15:05 -0300

    feat: adiciona formulario para metodo POST

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Button } from 'react-bootstrap'
+import { Table, Button, Form } from 'react-bootstrap'
 
 
 export class Users extends React.Component {
@@ -43,36 +43,78 @@ export class Users extends React.Component {
 
     render() {
         return (
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>E-mail</th>
-                        <th>Telefone</th>
-                        <th>idade</th>
-                        <th>Gênero</th>
-                        <th>Ocupação</th>
-                        <th> Editar</th>
+            <>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th>Telefone</th>
+                            <th>idade</th>
+                            <th>Gênero</th>
+                            <th>Ocupação</th>
+                            <th> Editar</th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.usuarios.map((usuarios) =>
-                            <tr>
-                                <td> {usuarios.nome} </td>
-                                <td> {usuarios.email} </td>
-                                <td> {usuarios.telefone} </td>
-                                <td> {usuarios.idade} </td>
-                                <td> {usuarios.genero}</td>
-                                <td> {usuarios.ocupacao} </td>
-                                <td> Atualizar  <Button variant="danger" onClick={() => this.deleteUser(usuarios.id)}>Excluir</Button></td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </Table>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.usuarios.map((usuarios) =>
+                                <tr>
+                                    <td> {usuarios.nome} </td>
+                                    <td> {usuarios.email} </td>
+                                    <td> {usuarios.telefone} </td>
+                                    <td> {usuarios.idade} </td>
+                                    <td> {usuarios.genero}</td>
+                                    <td> {usuarios.ocupacao} </td>
+                                    <td> Atualizar  <Button variant="danger" onClick={() => this.deleteUser(usuarios.id)}>Excluir</Button></td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </Table>
+                <div>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control type="email" placeholder="" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>E-mail</Form.Label>
+                            <Form.Control type="text" placeholder="" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Telefone</Form.Label>
+                            <Form.Control type="number" placeholder="" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Idade</Form.Label>
+                            <Form.Control type="number" placeholder="" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Gênero</Form.Label>
+                            <Form.Control type="text" placeholder="" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Ocupação</Form.Label>
+                            <Form.Control type="text" placeholder="" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Salvar
+                        </Button>
+                    </Form>
+                </div>
+            </>
         )
 
+
     }
+
+
 }
