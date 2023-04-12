8b0423b13b3a11ef18d4a9c75899f04dd3e0e3c9
israelsanttana <israelsantanasantos7@gmail.com>
2023-04-12 18:39:45 -0300

    fix: resolve conflito caminho Component Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Home } from './components/home';
-import { About } from './components/about';
+import { Home } from './components/Home';
+import { About } from './components/About';
 import { Users } from './components/users';
 import styles from './global.module.css'
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom'
